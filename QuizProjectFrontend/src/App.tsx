3a7ef0d020ad2f29c54a067ef49c258e94019065
Doorschot <danieloorschot@gmail.com>
2023-06-12 16:26:55 +1200

    Fixed Rendering issue

--- QuizProjectFrontend/src/App.tsx
+++ QuizProjectFrontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import countryJson from './countrydata.json';
@@ -10,12 +10,15 @@ const App = () => {
   let date = new Date().toUTCString().slice(5, 16);
   var todaysSeed = require('random-seed').create();
   todaysSeed.seed(date);
-  var todaysNumber = todaysSeed(countryJson.length + 1);
+  var todaysNumber = todaysSeed(countryJson.length);
 
   const [countryData, setCountryData] = useState<Country[]>([]);
-  setCountryData(countryJson);  
 
-  const blankCountry : Country = {
+  useEffect(() => {
+    setCountryData(countryJson);
+  }, [])
+
+  const blankCountry: Country = {
     name: '',
     continent: '',
     population: 0,
@@ -27,7 +30,7 @@ const App = () => {
   const [guessData, setGuessData] = useState(blankCountry);
 
   const makeGuess = (g: string) => {
-    const found = countryData.find(({name}) => name === g);
+    const found = countryData.find(({ name }) => name === g);
     if (found) {
       setGuessData(found);
     }
@@ -37,22 +40,24 @@ const App = () => {
 
   return (
     <div>
-        {countryData[todaysNumber].name}
-
-        <div>
+      <div>
+      {countryData[todaysNumber].name}
+    </div>
+        
+      <div>
         <label>
-        Guess:
-        <input
-          onChange={e => setGuess(e.target.value)}
-        />
-        <button onClick={() => makeGuess(guess)}>
-          Make Guess
-        </button>
-      </label>
-        </div>
-        <div>
-          {guessData.name}
-        </div>
+          Guess:
+          <input
+            onChange={e => setGuess(e.target.value)}
+          />
+          <button onClick={() => makeGuess(guess)}>
+            Make Guess
+          </button>
+        </label>
+      </div>
+      <div>
+        {guessData.name}
+      </div>
 
     </div>
   );
