8367f80ca4759d131c588a21fcba2fb0613108e2
Doorschot <danieloorschot@gmail.com>
2023-06-26 23:31:56 +1200

    Create WebScraper.py

---
+++ WebScraper.py
@@ -0,0 +1,9 @@
+import json
+import pip._vendor.requests as r
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
+dfs = pd 
+
