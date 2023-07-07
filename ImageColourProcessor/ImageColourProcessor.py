a2951b84b01b94a8729271449d3370069df3c905
Doorschot <danieloorschot@gmail.com>
2023-07-07 23:53:02 +1200

    Changed library and working image colour list
    Program now gets image colours and looks for the most popular colours (>5%) however this threshold is subject to change

--- ImageColourProcessor/ImageColourProcessor.py
+++ ImageColourProcessor/ImageColourProcessor.py
@@ -1,11 +1,20 @@
 
 if __name__ == '__main__':
-    import imageio
+    from PIL import Image
 
-    flag = imageio.v2.imread("Andorra.png")
-    print('Type of the image : ' , type(flag))
-    print()
-    print('Shape of the image : {}'.format(flag.shape))
-    print('Image Hight {}'.format(flag.shape[0]))
-    print('Image Width {}'.format(flag.shape[1]))
-    print('Dimension of Image {}'.format(flag.ndim))
+    with Image.open("Andorra.png") as im:
+        imrgb = im.convert('RGB')
+        colour_list = imrgb.getcolors()
+        colour_list.sort(reverse=True)
+        num_pixels = imrgb.size[0] * imrgb.size[1]
+        print(num_pixels)
+        final_list = []
+        for i in range(len(colour_list)):
+            #threshold for colour inclusion needs adjusting
+            if colour_list[i][0] > (num_pixels/20):
+                final_list.append(colour_list[i])
+        print(final_list)
+
+
+    
+    
