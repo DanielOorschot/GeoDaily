299275517e8d22d56e68e034d0e156d7d26f0fc2
Doorschot <danieloorschot@gmail.com>
2023-07-08 17:32:57 +1200

    Changes to look at all pngs in folder

--- ImageColourProcessor/ImageColourProcessor.py
+++ ImageColourProcessor/ImageColourProcessor.py
@@ -1,19 +1,23 @@
 
 if __name__ == '__main__':
     from PIL import Image
+    import glob, os
 
-    with Image.open("Andorra.png") as im:
-        imrgb = im.convert('RGB')
-        colour_list = imrgb.getcolors()
-        colour_list.sort(reverse=True)
-        num_pixels = imrgb.size[0] * imrgb.size[1]
-        print(num_pixels)
-        final_list = []
-        for i in range(len(colour_list)):
-            #threshold for colour inclusion needs adjusting
-            if colour_list[i][0] > (num_pixels/20):
-                final_list.append(colour_list[i])
-        print(final_list)
+    #open all
+    flag_info = {}
+    for infile in glob.glob("*.png"):
+        file, ext = os.path.splitext(infile)
+        with Image.open(infile) as im:
+            imrgb = im.convert('RGB')
+            colour_list = imrgb.getcolors()
+            num_pixels = imrgb.size[0] * imrgb.size[1]
+            final_list = []
+            for i in range(len(colour_list)):
+                #threshold for colour inclusion needs adjusting
+                if colour_list[i][0] > (num_pixels/20):
+                    final_list.append(colour_list[i])
+            flag_info[file] = final_list
+    print(flag_info)
 
 
     
