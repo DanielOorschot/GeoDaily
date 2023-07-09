ce26d45e8540fcf4cb9b72a27de294e8de0b25c4
Doorschot <danieloorschot@gmail.com>
2023-07-09 23:53:44 +1200

    Add ability to look for closest colour match

--- ImageColourProcessor/ImageColourProcessor.py
+++ ImageColourProcessor/ImageColourProcessor.py
@@ -3,8 +3,13 @@ if __name__ == '__main__':
     from PIL import Image
     import glob, os
 
+    #codes are based on html colour list
+    colour_rgb_list = ["yellow","blue","red","green", "white","black"]
+    colour_reference = [(255, 255, 0),(0, 0, 255),(255, 0, 0),(0, 128, 0),(255,255,255),(0,0,0)]
+
     #open all
     flag_info = {}
+    flag_colour_info = {}
     for infile in glob.glob("*.png"):
         file, ext = os.path.splitext(infile)
         with Image.open(infile) as im:
@@ -16,6 +21,9 @@ if __name__ == '__main__':
                 #threshold for colour inclusion needs adjusting
                 if colour_list[i][0] > (num_pixels/20):
                     final_list.append(colour_list[i])
+                    print(colour_list[i])
+                    closest_colour = min(colour_reference, key=lambda c: (c[0]-colour_list[i][1][0])**2 + (c[1]-colour_list[i][1][1])**2 + (c[2]-colour_list[i][1][2])**2)
+                    print(closest_colour)
             flag_info[file] = final_list
     print(flag_info)
 
